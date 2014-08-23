6389e2865036d15b39eb75eb7936f469bc3d0a9c
Raju Gottumukkala <naniglavs@gmail.com>
2014-08-23 22:00:24 +0800

    Updated make both functions with working code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions makeCacheMatrix and cacheSolve, cache the inverse of a matrix
 
-## Write a short comment describing this function
+
+## This function creates a special matrix object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  # Set the inverse to NULL
+  inv <- NULL
+  
+  # This function sets the matrix and sets inverse to NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  # This function gets the matrix
+  get <- function() x
+  
+  # This function sets the inverse of the matrix
+  setinverse <- function(inverse) inv <<- inverse
+  
+  # This function gets the inverse of the matrix
+  getinverse <- function() inv
+  
+  # Return the list with the set, get, setinve
+  rse, getinverse functions that are to be used by cacheSolve
+  # function
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the matrix which is returned by the function 
+## makeCachematrix above. If the inverse has already been calculated (and the matrix 
+## has not changed), then the cachesolve will retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  # Get the inverse from x
+  inv <- x$getinverse()
+  
+  # Check if the inverse exists already. If so, return the inverse.
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)    
+  }
+  
+  # Following code runs if no previous inverse exists
+  
+  # Get the matrix
+  data <- x$get()
+  
+  # Calculate the inverse of the matrix (assuming matrix is convertible)
+  inv <- solve(data, ...)
+    message("calculating the inverse")
+  
+  # Set the inverse
+  x$setinverse(inv)
+  
+  ## Return the inverse matrix
+  inv
+    
 }
